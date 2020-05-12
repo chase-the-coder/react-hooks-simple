958521e75a8d16a3765f6278f8efba3b5ff4d418
chase lindsay <chase.lindsay1@gmail.com>
2020-05-11 22:44:52 -0400

    refactored resource list into functional component

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,22 +1,9 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
+import useResources from './useResources';
 
-export default class ResourceList extends Component {
-	state = { resources: [] };
+const ResourceList = ({ resource }) => {
+	const resources = useResources(resource);
 
-	async componentDidMount() {
-		const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
-		this.setState({ resources: response.data });
-	}
-
-	async componentDidUpdate(prevProps) {
-		if (prevProps.resource !== this.props.resource) {
-			const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
-			this.setState({ resources: response.data });
-		}
-	}
-
-	render() {
-		return <div>{this.state.resources.length}</div>;
-	}
-}
+	return <ul>{resources.map((record) => <li key={record.id}>{record.title}</li>)}</ul>;
+};
+export default ResourceList;
